a8c244032a6cf56895beb74788340ce8aea9d7ec
Augusto Castro <augustocesarcastro026@gmail.com>
2021-05-17 18:43:17 -0300

    setUpRecyclerView added. Visibility UI updated

--- app/src/main/java/com/example/noteapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/HomeFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.noteapp.MainActivity
 import com.example.noteapp.R
+import com.example.noteapp.adapter.NoteAdapter
 import com.example.noteapp.databinding.FragmentHomeBinding
+import com.example.noteapp.model.Note
+import com.example.noteapp.viewmodel.NoteViewModel
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +44,55 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView()
+        noteViewModel = (activity as MainActivity).noteViewModel
 
         binding.fabAddNote.setOnClickListener{ mView ->
 
             mView.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
-
-
         }
     }
+
+    private fun setUpRecyclerView(){
+       noteAdapter = NoteAdapter()
+
+       binding.recyclerView.apply{
+
+           layoutManager = StaggeredGridLayoutManager(
+               2,
+               StaggeredGridLayoutManager.VERTICAL
+           )
+
+           setHasFixedSize(true)
+           adapter = noteAdapter
+       }
+
+        activity?.let {
+            noteViewModel.getAllNote().observe(viewLifecycleOwner, {note ->
+                noteAdapter.differ.submitList(note)
+                updateUI(note)
+            })
+        }
+    }
+
+    private fun updateUI(note: List<Note>){
+
+        if(note.isNotEmpty()){
+
+            binding.recyclerView.visibility = View.VISIBLE
+            binding.tvNoNotesAvailable.visibility = View.GONE
+        } else {
+            binding.recyclerView.visibility = View.GONE
+            binding.tvNoNotesAvailable.visibility = View.VISIBLE
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.home_menu, menu)
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
