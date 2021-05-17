6a50a980d03508d518bb3d6aa07236434a60e68a
Augusto Castro <augustocesarcastro026@gmail.com>
2021-05-17 18:27:49 -0300

    added saveNote method and view integration

--- app/src/main/java/com/example/noteapp/fragments/NewNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/NewNoteFragment.kt
@@ -2,15 +2,26 @@ package com.example.noteapp.fragments
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.noteapp.MainActivity
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentNewNoteBinding
+import com.example.noteapp.model.Note
+import com.example.noteapp.toast
+import com.example.noteapp.viewmodel.NoteViewModel
+import com.google.android.material.snackbar.Snackbar
 
 
 class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
 
     private var _binding: FragmentNewNoteBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var noteViewModel: NoteViewModel
+    private lateinit var mView: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +40,42 @@ class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        noteViewModel = (activity as MainActivity).noteViewModel
+        mView = view
+    }
+
+    private fun saveNote(view: View){
+
+        val noteTitle = binding.etNoteTitle.text.toString().trim()
+        val noteBody = binding.etNoteBody.text.toString().trim()
+
+        if(noteTitle.isNotEmpty()){
+            val note = Note (0, noteTitle, noteBody)
+
+            noteViewModel.addNote(note)
+            Snackbar.make(view, "Note saved successfully", Snackbar.LENGTH_SHORT).show()
+
+            view.findNavController().navigate(
+                R.id.action_newNoteFragment_to_homeFragment
+            )
+        } else {
+            activity?.toast(" Please enter note title!")
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.save_menu -> {
+                saveNote(mView)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroy() {
